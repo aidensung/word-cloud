7c222f4f18e3a4ab4195ca7b741ee52b9cd8e901
Ilhyo Sung <sih8189@gmail.com>
2020-03-18 02:36:51 -0400

    added Firebase database and Word [View, Add, Delete]

--- src/components/AppShell.js
+++ src/components/AppShell.js
@@ -6,7 +6,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Drawer from "@material-ui/core/Drawer";
 import MenuItem from "@material-ui/core/MenuItem";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/core/Menu";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const styles = {
   root: {
