2c25b8a5908c915feb10c24f14c04418b7115204
Ilhyo Sung <sih8189@gmail.com>
2020-03-18 00:27:17 -0400

    added Router Module

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <Card>
+        <CardContent>Word Cloud App using React & Firebase</CardContent>
+      </Card>
+    );
+  }
+}
+
+export default Home;
