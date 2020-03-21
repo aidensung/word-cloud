2206788e55ce49c7e3c3d79d863976f2a6e1d4db
Ilhyo Sung <sih8189@gmail.com>
2020-03-21 09:40:20 -0400

    fixed apiURL

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -13,7 +13,7 @@ import DialogContent from "@material-ui/core/DialogContent";
 import TextField from "@material-ui/core/TextField";
 
 const databaseURL = "https://word-cloud-a581f.firebaseio.com/";
-const apiURL = "http://word-cloud-aiden.com/";
+const apiURL = "https://word-cloud-aiden.com/";
 
 const styles = theme => ({
   fab: {
